7f6f7a7bc02b25857329e29ce55f4d89c460106d
patricklapgar <patricklapgardeveloper@gmail.com>
2020-02-29 01:27:53 -0600

    initial commit

---
+++ search.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Zeebie</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    </head>
+    <body>
+        <div class="wrapper">
+
+            <div class="header">
+                <div class="headerContent">
+                    <div class="logoContainer">
+                        <a href="index.php">
+                            <img src="assets/images/zeebeeLogo.png" alt="">
+                        </a>
+                    </div>
+
+                    <div class="searchContainer">
+                        <form action="search.php" method="GET">
+                            <div class="searchBarContainer">
+                                <input type="text" class="searchBox" name="term">
+                                <button>
+                                    Search
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </body>
+</html>
