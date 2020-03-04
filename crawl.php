4a3f38b2dfbe8ac2c2d0400ca3f3158879a9fabf
patricklapgar <patricklapgardeveloper@gmail.com>
2020-03-04 01:24:47 -0600

    Converted relative links to absolute links with the 'createLink' function. Now the 'followLinks' function is being used to recursively crawl through each link it finds on a specific web page. Soon, each link will be inserted into a local DB

--- crawl.php
+++ crawl.php
@@ -1,11 +1,52 @@
 <?php 
     include("classes/DomDocumentParser.php");
 
+    /* 
+        IMPORTANT TO NOTE:
+        An important feature of the web crawler will be to recursively crawl absolute links
+        When a link appears on a page, the web crawler will go to that link and then crawl other links on that page and return to the previous page
+        Then, if there are more links on the original page, the crawler will continue to the remaining and crawl through those as well
+        Each link it finds will be recursively crawled and continue until there are no more links to crawl
+    */
+
+    // Array of links already crawled
+    $alreadyCrawled = array();
+
+    // Array of links that need to be crawled
+    $crawling = array();
+
+    // This function will convert relative links to absolute links
     function createLink($src, $url) {
+        $scheme = parse_url($url)["scheme"]; // http
+        $host = parse_url($url)["host"]; //host
+
+        if(substr($src, 0, 2) == "//"){
+            $src = $scheme . ":" . $src;
+
+        } else if(substr($src, 0, 1) == "/") {
+            $src = $scheme . "://" . $host . $src;
         
+        } else if(substr($src, 0, 2) == "./") {
+            // If the url is calling to a different directory, parse the url and find the directory name to append it to the src variable
+            $src = $scheme . "://" . $host . dirname(parse_url($url)["path"]) . substr($src, 1);
+        
+        } else if(substr($src, 0, 3) == "../") {
+            $src = $scheme . "://" . $host . "/" > $src;
+
+        } else if(substr($src, 0, 5) != "https" && substr($src, 0, 4) != "http") {
+            $src = $scheme . "://" . $host . "/" > $src;
+
+        }
+
+        return $src;
     }
 
     function followLinks($url) {
+
+        // Specify the two arrays above are global
+        global $alreadyCrawled;
+        global $crawling;
+
         $parser = new DomDocumentParser($url);
         $linkList = $parser->getLinks();
 
@@ -19,9 +60,23 @@
                 continue;
             }
 
-            createLink($href, $url);
+            $href = createLink($href, $url);
 
-            // echo $href . "<br>";
+            if(!in_array($href, $alreadyCrawled)) {
+                // The link will be put into the next index of the 'alreadyCrawled' and 'crawling' arrays
+                $alreadyCrawled[] = $href;
+                $crawling[] = $href;
+
+                // TODO: Insert href tags into local DB
+            }
+
+            echo $href . "<br>";
+        }
+
+        array_shift($crawling);
+
+        foreach($crawling as $site) {
+            followLinks($site);
         }
     }
 
