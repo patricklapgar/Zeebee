62fc8ae74e77a07085dddeaae933bffb5321e3eb
patricklapgar <patricklapgardeveloper@gmail.com>
2020-03-13 02:10:40 -0500

    Site results class now fetches data associated with the user's search term such as the related URLs, titles, and descriptions.

--- search.php
+++ search.php
@@ -51,10 +51,14 @@
                 </div>
             </div>
 
-            <div class="mainReseultsSection">
+            <div class="mainResultsSection">
                 <?php
                     $resultsProvider = new SiteResultsProvider($con);
-                    echo $resultsProvider->getNumResults($term);
+                    $numResults = $resultsProvider->getNumResults($term);
+                    
+                    echo "<p class='resultsCount'>$numResults results found</p>";
+
+                    echo $resultsProvider->getResultsHtml(1, 20, $term);
                 ?>
             </div>
             
